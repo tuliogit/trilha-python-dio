cd3443cf148e7bbc1f3f1a02fb2af9925c4a55e8
Túlio Simões Martins Padilha <wikigeolog@gmx.com>
2025-04-15 11:22:13 -0300

    Update desafio.py

--- 00 - Fundamentos/desafio.py
+++ 00 - Fundamentos/desafio.py
@@ -18,7 +18,7 @@ while True:
     opcao = input(menu)
 
     if opcao == "d":
-        valor = float(input("Informe o valor do depósito: "))
+        valor = float(input("Informe o valor a ser depositado: "))
 
         if valor > 0:
             saldo += valor
